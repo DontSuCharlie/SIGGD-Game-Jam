4b45e91e98c7c8b36dc5462d47cfca02ebc7e8f2
Daniel Hewlett <DanCHewlett@gmail.com>
2015-09-26 16:59:00 -0400

    yee

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -17,97 +17,110 @@ public class PlayerControl : MonoBehaviour {
 	public float moveDelay = 2f;
 	public float walkDistance = 1;
 	private bool canRotate = true;
-	public float maxWalkerDist = 4f;
+	public float maxWalkerDist = 3.5f;
+	public bool alive = true;
 	//public float timer = 0;
 
 	// Use this for initialization
 	void Start () {
-		playerRB = GetComponent<Rigidbody>();
-		walker = GameObject.Find ("Walker");
-		walkerMoveTarget = GameObject.Find ("WalkerMoveTarget");
-		walkerRB = walker.GetComponent<Rigidbody>();
-		cam = GameObject.Find ("PlayerCam");
-        //InvokeRepeating ("inputHandeling", 0f, 1f);
-        StartCoroutine("moveAll");
+			playerRB = GetComponent<Rigidbody> ();
+			walker = GameObject.Find ("Walker");
+			walkerMoveTarget = GameObject.Find ("WalkerMoveTarget");
+			walkerRB = walker.GetComponent<Rigidbody> ();
+			cam = GameObject.Find ("PlayerCam");
+			//InvokeRepeating ("inputHandeling", 0f, 1f);
+			StartCoroutine ("moveAll");
     }
 
     // Update is called once per frame
     void Update () {
-		if (Input.GetButtonDown("WalkForward")) {
-			walk = true;
-		}
-		if (Input.GetButtonUp("WalkForward")) {
-			walk = false;
-		}
-		if (Input.GetButtonDown("Grab")) {
-			RaycastHit hit;
-			if (Physics.Raycast(cam.transform.position, cam.transform.TransformDirection(0,0,1))) {
+		if (alive) {
+			if (Input.GetButtonDown ("WalkForward")) {
+				walk = true;
 			}
-		}
-		if (Input.GetButtonDown("Push")) {
-			thrustWalker();
+			if (Input.GetButtonUp ("WalkForward")) {
+				walk = false;
+			}
+			if (Input.GetButtonDown ("Grab")) {
+				RaycastHit hit;
+				if (Physics.Raycast (cam.transform.position, cam.transform.TransformDirection (0, 0, 1))) {
+				}
+			}
+			if (Input.GetButtonDown ("Push")) {
+				thrustWalker ();
+			}
 		}
 
 	}
 
 
 	void FixedUpdate() {
-		//limit player speed
-		if (playerRB.velocity.magnitude > maxSpeed) {
-			playerRB.velocity = playerRB.velocity.normalized * maxSpeed;
-		}
-
-		//check if too far from walker
-		tooFarFromWalker ();
-
-		if (Vector3.Distance (transform.position, walkerMoveTarget.transform.position) < walkDistance + 1) {
-			canRotate = true;
-		} else {
-			canRotate = false;
-		}
-
-		//--person forward movement--
-		Vector3 player2DPos = new Vector3 (transform.position.x, 0, transform.position.z);
-		Vector3 walker2DPos = new Vector3 (walkerMoveTarget.transform.position.x, 0, walkerMoveTarget.transform.position.z);
-		//if (Vector3.Distance (transform.position, walkerMoveTarget.transform.position) > walkDistance) {
-		if (Vector3.Distance (player2DPos, walker2DPos) > walkDistance) {
-			playerRB.AddRelativeForce (new Vector3(0, 0, walkSpeed), ForceMode.VelocityChange);
-		} else {
-			playerRB.AddForce (Vector3.zero, ForceMode.VelocityChange);
-		}
-		//--player rotation--
-		//turn right
-		if (Input.GetAxis ("Horizontal") > 0) {
-
-			if (!rightBlocked && !walk && canRotate) {
-				transform.Rotate (0, turnSpeed, 0);
+		if (alive) {
+			//limit player speed
+			if (playerRB.velocity.magnitude > maxSpeed) {
+				playerRB.velocity = playerRB.velocity.normalized * maxSpeed;
 			}
-		}
-		//turn left
-		if (Input.GetAxis ("Horizontal") < 0) {
 
-			if (!leftBlocked && !walk && canRotate) {
-				transform.Rotate (0, -turnSpeed, 0);
+			//check if too far from walker
+			tooFarFromWalker ();
+
+			if (Vector3.Distance (transform.position, walkerMoveTarget.transform.position) < walkDistance + 1) {
+				canRotate = true;
+			} else {
+				canRotate = false;
 			}
-		}
-		//--camera rotation--
-		float yRot = cam.transform.localRotation.eulerAngles.y;
-		float xRot = cam.transform.localRotation.eulerAngles.x;
-		float newYRot = Input.GetAxis("Mouse X")  + yRot;
-		float newXRot = (-Input.GetAxis("Mouse Y"))  + xRot;
-		//Debug.Log (newYRot);
-		Debug.Log (Input.GetAxis ("Mouse Y"));
-		//limit horizontal rotation
-		if ((newYRot > 65 && newYRot < 325) || (newYRot > 180 && newYRot < 65)) {
+
+			//--person forward movement--
+			Vector3 player2DPos = new Vector3 (transform.position.x, 0, transform.position.z);
+			Vector3 walker2DPos = new Vector3 (walkerMoveTarget.transform.position.x, 0, walkerMoveTarget.transform.position.z);
+			//if (Vector3.Distance (transform.position, walkerMoveTarget.transform.position) > walkDistance) {
+			if (Vector3.Distance (player2DPos, walker2DPos) > walkDistance) {
+				//Ray dir = new Ray(transform.position, walkerMoveTarget.transform.position);
+				//playerRB.AddForce((dir.direction) * maxSpeed, ForceMode.VelocityChange);
+				//Quaternion look = Quaternion.LookRotation (walker.transform.position);
+				playerRB.AddRelativeForce (new Vector3 (0, 0, walkSpeed), ForceMode.VelocityChange);
+				//playerRB.MoveRotation (Quaternion.Euler (new Vector3 (0, look.eulerAngles.y, 0)));
+				//playerRB.AddRelativeForce (new Vector3 (0, 0, walkSpeed), ForceMode.VelocityChange);
+
+				//playerRB.AddForce (new Vector3(walkerMoveTarget.transform.position.x, 0, walkerMoveTarget.transform.position.z), ForceMode.VelocityChange);
+			} else {
+				playerRB.AddForce (Vector3.zero, ForceMode.VelocityChange);
+			}
+			//--player rotation--
+			//turn right
+			if (Input.GetAxis ("Horizontal") > 0) {
+
+				if (!rightBlocked && !walk && canRotate) {
+					transform.Rotate (0, turnSpeed, 0);
+				}
+			}
+			//turn left
+			if (Input.GetAxis ("Horizontal") < 0) {
+
+				if (!leftBlocked && !walk && canRotate) {
+					transform.Rotate (0, -turnSpeed, 0);
+				}
+			}
+			//--camera rotation--
+			float yRot = cam.transform.localRotation.eulerAngles.y;
+			float xRot = cam.transform.localRotation.eulerAngles.x;
+			float newYRot = Input.GetAxis ("Mouse X") + yRot;
+			float newXRot = (-Input.GetAxis ("Mouse Y")) + xRot;
 			//Debug.Log (newYRot);
-			newYRot = yRot;
+			Debug.Log (Input.GetAxis ("Mouse Y"));
+			//limit horizontal rotation
+			if ((newYRot > 65 && newYRot < 325) || (newYRot > 180 && newYRot < 65)) {
+				//Debug.Log (newYRot);
+				newYRot = yRot;
+			}
+			//limit vertical rotation
+			if ((newXRot > 5 && newXRot < 180) || (newXRot < 340 && newXRot > 275)) {
+				//Debug.Log (newXRot);
+				newXRot = xRot;
+			}
+			cam.transform.localEulerAngles = new Vector3 (newXRot, newYRot, 0);
 		}
-		//limit vertical rotation
-		if ((newXRot > 5 && newXRot < 180) || (newXRot < 340 && newXRot > 275)) {
-			//Debug.Log (newXRot);
-			newXRot = xRot;
-		}
-		cam.transform.localEulerAngles = new Vector3 (newXRot, newYRot, 0);
+
 	}
 
 	public void inputHandeling() {
@@ -173,7 +186,9 @@ public class PlayerControl : MonoBehaviour {
 
 	void tooFarFromWalker() {
 		if (Vector3.Distance (transform.position, walkerMoveTarget.transform.position) > maxWalkerDist) {
+			playerRB.velocity = Vector3.zero;
 			playerRB.constraints = RigidbodyConstraints.None;
+			alive = false;
 		}
 	}
     
