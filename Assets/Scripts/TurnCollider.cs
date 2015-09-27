7cdc07268da37287664dd4b3a55cff11b5e66078
hpoggie <henry.poggie@gmail.com>
2015-09-27 14:24:37 -0400

    Added turn collider but didn't use it

---
+++ Assets/Scripts/TurnCollider.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Side {
+	Left,
+	Right
+}
+
+public class TurnCollider : MonoBehaviour {
+	private PlayerControl controller;
+	private Side side = Side.Left;
+
+	void Start () {
+		controller = GetComponent<PlayerControl>();
+	}
+	
+	void OnCollisionEnter (Collision col) {
+		//if (side == Side.Left) controller.leftIsBlocked();
+		//else if (side == Side.Right) controller.rightIsBlocked();
+		return;
+	}
+
+	void OnCollisionExit (Collision col) {
+		//if (side == Side.Left) controller.leftIsNotBlocked();
+		//else if (side == Side.Right) controller.rightIsNotBlocked();
+		return;
+	}
+}
