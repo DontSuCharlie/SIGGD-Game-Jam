91e9ef3ff442ad137a26629f795deeeafe95d544
hpoggie <henry.poggie@gmail.com>
2015-09-27 12:35:52 -0400

    Added blinky light

---
+++ Assets/Scripts/FlashingLight.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashingLight : MonoBehaviour {
+	private Light light;
+	private AudioSource source;
+	private float[] samples = new float[64];
+
+	void Start () {
+		source = GetComponent<AudioSource>();
+		light = GetComponent<Light>();
+	}
+	
+	void Update () {
+		source.GetSpectrumData(samples, 0, FFTWindow.Rectangular);
+		light.intensity = samples[0] * 7 + 1;
+	}
+}
